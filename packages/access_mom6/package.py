9a43e32ab24ca087ec4d45efe0817e14142a10de
Harshula Jayasuriya <harshula.jayasuriya@anu.edu.au>
2025-08-04 14:54:59 +1000

    Support Spack v1.0 by depending on languages

--- packages/access_mom6/package.py
+++ packages/access_mom6/package.py
@@ -43,6 +43,9 @@ class AccessMom6(CMakePackage):
         when="@2025.07.001:"
     )
 
+    depends_on("c", type="build")
+    depends_on("fortran", type="build")
+
     depends_on("access3-share", when="+access3")
     depends_on("cmake@3.18:", type="build")
     depends_on("mpi")
