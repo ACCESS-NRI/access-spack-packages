34a9f2d1814343248c4e2b8c7487c18d358fa29e
Lachlan Whyborn <lachlan.whyborn@anu.edu.au>
2026-02-10 16:19:01 +1100

    Fix missing bracket

--- spack_repo/access/nri/packages/um/package.py
+++ spack_repo/access/nri/packages/um/package.py
@@ -187,6 +187,7 @@ class Um(Package):
             "git_url": "https://github.com/ACCESS-NRI/ukca.git",
             "subdir": "ukca"
             }
+        }
 
     def _config_file_path(self, model):
         """
