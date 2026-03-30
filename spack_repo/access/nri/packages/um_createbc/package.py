f54b2909ead33f2953006649a9ecf9f1589fb54b
Paul Leopardi <paul.leopardi@anu.edu.au>
2026-03-31 09:54:43 +1100

    Refactor the um SPR to build from GitHub, and add an SPR for um-createbc
    * Begin refactoring the UM packages by creating um_base.py
    
    * Refactor the Um class to use UmBasePackage
    
    * Add a refactored version of um_createbc
    
    * Introduce the UmBasePackage class
    
    * Refactor UmCreatebc
    
    * Try patching the gcom serial build
    
    * Optionally use GitHub for model subcomponents
    
    * Use corrected default tags
    
    * Set config_root_path to UM resource_path as needed
    
    * Improve documentation
    
    * Add _github_models and resource helper functions
    
    * Use _resources_needed
    
    * Use spack.fetch_strategy
    
    * Avoid a recursion loop
    
    * Update the README doc
    
    * Delete an unused variable
    
    * Change the preferred version to 13.8
    
    * On CI failure, print debug into to terminal
    
    * Revert the change to ci.yml
    
    * Do not exclude "um_ref" from patch
    
    * Add explanations to README.md
    
    * Temporarily switch on maximum verbosity
    
    * Fix a missing comma
    
    * Temporarily set allow-ssh-into-spack-install: true
    
    * Set allow-ssh-into-spack-install: false again
    
    * Try allow-ssh-into-spack-install: true again
    
    * If GitHub is used, set the _project_location
    
    * Remove temporary debugging
    
    * Try a dynamic fcm-make.cfg
    
    * Get subdir from the right place
    
    * Use self._resource_cfg
    
    * Bring the doc up to date
    
    * Test both um and um-createbc using both gcc and intel
    
    * Refactor to use projects in _resource_cfg
    
    * Update README
    
    * Fix a typo that added an extra space
    
    * Temporarily set allow-ssh-into-spack-install true
    
    * Set allow-ssh-into-spack-install back to false
    
    * Do not test um or um-createbc using gcc
    
    * Refactor project-based collections
    
    Also move gcom depends to the UM package
    
    * Consolidate loops. Make build_dir public.
    
    * build_dir is now public.
    
    * Use Spack idioms
    
    * Set extract.location to the dynamic resource path
    
    * Update copyright and URL comments
    
    * Bring README up to date
    
    * Remove install_bin_dir
    
    * Use "project" rather than "resource" consistently
    
    * Add a variable for the dynamic config path. Improve comments.
    
    * Correct one comment
    
    * Describe the cleanup once all models use GitHub
    
    * Fix a URL in a comment
    
    * Add comments showing examples of tags.
    
    * Remove jules_sources from _other_variants

---
+++ spack_repo/access/nri/packages/um_createbc/package.py
@@ -0,0 +1,52 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# Copyright 2024-2026 ACCESS-NRI
+
+from spack_repo.access.nri.build_systems.um_base import UmBasePackage
+from spack.package import *
+
+class UmCreatebc(UmBasePackage):
+    """
+    UmCreatebc creates local boundary conditions for the UM weather and climate model.
+    """
+
+    variant("model", default="vn13", description="Model configuration.",
+        values=("vn13", "vn13p5-rns"), multi=False)
+
+    # List of model variants that have been migrated to Github sources.
+    # Defined in parent class and overridden here.
+    github_models = ("vn13",)
+
+    # List of projects to be used by this package.
+    # Defined in parent class and overridden here.
+    projects_needed = ("shumlib", "um")
+
+    # For GCOM versions, see
+    # https://code.metoffice.gov.uk/trac/gcom/wiki/Gcom_meto_installed_versions
+    # um_createbc uses the ~mpi variants of gcom exclusively.
+    depends_on("gcom~mpi@7.8", when="@:13.0", type=("build", "link"))
+    depends_on("gcom~mpi@7.9", when="@13.1", type=("build", "link"))
+    depends_on("gcom~mpi@8.0", when="@13.2", type=("build", "link"))
+    depends_on("gcom~mpi@8.1", when="@13.3", type=("build", "link"))
+    depends_on("gcom~mpi@8.2", when="@13.4", type=("build", "link"))
+    depends_on("gcom~mpi@8.3", when="@13.5:13.7", type=("build", "link"))
+    depends_on("gcom~mpi@8.4", when="@13.8", type=("build", "link"))
+    depends_on("gcom~mpi@8.4:", when="@13.9:", type=("build", "link"))
+
+    def setup_run_environment(self, env):
+        """
+        Set the built path into the environment.
+        """
+        # Add the built executables to the path
+        env.prepend_path("PATH", self.prefix.bin)
+
+
+    def install(self, spec, prefix):
+        """
+        Install executables and accompanying files into the prefix directory.
+        """
+        build_bin_dir = join_path(self.build_dir(), "build-createbc", "bin")
+        mkdirp(prefix.bin)
+        install_tree(build_bin_dir, prefix.bin)
