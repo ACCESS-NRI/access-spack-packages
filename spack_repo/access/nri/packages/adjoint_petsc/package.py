e2d80414e29bc26fc30eb433e45d6d3a351c8f03
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2026-04-13 13:25:45 +1000

    amend paths

--- spack_repo/access/nri/packages/adjoint_petsc/package.py
+++ spack_repo/access/nri/packages/adjoint_petsc/package.py
@@ -28,13 +28,13 @@ class AdjointPetsc(CMakePackage):
 
     license("LGPL-3.0-only")
 
-    version("master", branch="master")
+    version("main", branch="master")
 
     variant("shared", default=True, description="Build shared libraries")
     variant("examples", default=False, description="Build examples")
     variant("tests", default=False, description="Build tests")
 
-    depends_on("cmake@3.16:", type="build")
+    depends_on("cmake@3.20:", type="build")
     depends_on("pkgconfig", type="build")
     depends_on("cxx", type="build")
 
@@ -51,7 +51,7 @@ class AdjointPetsc(CMakePackage):
             self.define_from_variant("BUILD_SHARED_LIBS", "shared"),
             self.define_from_variant("BUILD_EXAMPLES", "examples"),
             self.define_from_variant("BUILD_TESTING", "tests"),
-            self.define("CoDiPack_DIR", self.spec["codipack"].prefix),
+            self.define("CoDiPack_DIR", join_path(self.spec["codipack"].prefix, "share", "CoDiPack", "cmake")),
             self.define("PETSc_DIR", self.spec["petsc"].prefix),
             self.define("CMAKE_CXX_STANDARD", 23),
             self.define("CMAKE_CXX_STANDARD_REQUIRED", True),
