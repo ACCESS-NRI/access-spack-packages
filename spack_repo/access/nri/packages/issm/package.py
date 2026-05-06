8060176348bd871034e9d876670d64c0c0281251
Justin Kin Jun Hew <justinkinjun.hew@anu.edu.au>
2026-05-06 15:48:25 +1000

    mistake

--- spack_repo/access/nri/packages/issm/package.py
+++ spack_repo/access/nri/packages/issm/package.py
@@ -223,7 +223,7 @@ class Issm(AutotoolsPackage):
             ]
         args.append(f"--with-parmetis-dir={self.spec['parmetis'].prefix}")
         args.append(f"--with-metis-dir={self.spec['metis'].prefix}")
-        args.append(f"--with-mumps-dir={self.spec['access-mumps'].prefix}")
+        args.append(f"--with-mumps-dir={self.spec['mumps'].prefix}")
 
         # Optimiser
         args.append(f"--with-m1qn3-dir={self.spec['m1qn3'].prefix.lib}")
