66a2f8590bcf487605f41382dc881a0d5f995fe7
Martin Dix <martin.dix@anu.edu.au>
2026-07-24 17:27:08 +1000

    Enable GCOM4 gfortran build

--- spack_repo/access/nri/packages/gcom4/package.py
+++ spack_repo/access/nri/packages/gcom4/package.py
@@ -33,8 +33,6 @@ class Gcom4(Package):
     depends_on("fcm", type="build")
     depends_on("mpi", when="+mpi")
 
-    conflicts("%gcc")
-
     def gcom_machine(self, spec):
         """
         Determine the machine configuration name
@@ -43,11 +41,8 @@ class Gcom4(Package):
             mach_c = "ifort"
         elif spec.satisfies("%oneapi"):
             mach_c = "oneapi"
-        # https://github.com/ACCESS-NRI/access-spack-packages/issues/85
-        # versions 4.7 and 4.8 support the gcc Fortran compiler,
-        # but versions 4.5 and 4.6 do not.
-        # elif spec.satisfies("%gcc"):
-        #     mach_c = "gfortran"
+        elif spec.satisfies("%gcc"):
+            mach_c = "gfortran"
         else:
             raise NotImplementedError("Unknown compiler")
         if "+mpi" in spec:
